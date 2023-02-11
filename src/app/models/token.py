9701a6a6c993a5aa3d093cdcfcec3a8f547d3a9f
Arunanshu Biswas <mydellpc07@gmail.com>
2023-02-11 17:26:45 +0530

    feat(token): add token schemas

---
+++ src/app/models/token.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from sqlmodel import SQLModel
+
+
+class Token(SQLModel):
+    """Represents a token as sent to the user."""
+
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(SQLModel):
+    """The token contents."""
+
+    sub: int | None
