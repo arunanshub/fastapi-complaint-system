ffa0d5189af7ba7d604080ab032c1f509255ceb1
Arunanshu Biswas <mydellpc07@gmail.com>
2023-02-12 19:23:50 +0530

    docs(database): add docs for `get_db`

--- src/app/database.py
+++ src/app/database.py
@@ -11,5 +11,12 @@ engine = create_async_engine(settings.DATABASE_URL)
 
 
 async def get_db() -> typing.AsyncIterable[AsyncSession]:
+    """A convenience function to create one time database session.
+
+    It is generally used with ``fastapi.Depends`` object.
+
+    Returns:
+        Asynchronous iterable with a single asynchronous session.
+    """
     async with AsyncSession(engine) as session:
         yield session
