6fb3fd4d79ba7bbc63dbb65b3b7f8b5c428c22a5
Arunanshu Biswas <mydellpc07@gmail.com>
2023-02-14 02:14:08 +0530

    feat(api/user): add administrative user fetching/listing function

--- src/app/api/user.py
+++ src/app/api/user.py
@@ -1,16 +1,32 @@
 from __future__ import annotations
 
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import EmailStr  # noqa: TC002
 from sqlmodel.ext.asyncio.session import AsyncSession  # noqa: TC002
 from starlette import status
 
-from ..api.deps import get_current_user
+from ..api.deps import get_current_admin, get_current_user
 from ..crud import user
 from ..database import get_db
 from ..exc import NotUniqueError
 from ..models.user import User, UserCreate, UserRead
 
 router = APIRouter()
+
+
+@router.get(
+    "/",
+    response_model=list[UserRead],
+    dependencies=[Depends(get_current_admin)],
+)
+async def get_users(
+    email: EmailStr | None = None,
+    db: AsyncSession = Depends(get_db),
+) -> list[User]:
+    if email is None:
+        return await user.get_multi(db)
+    db_user = await user.get_by_email(db, email=email)
+    return [db_user] if db_user else []
 
 
 @router.get("/me", response_model=UserRead)
