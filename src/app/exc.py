0b14da542a74fa40155eafbcc78e8dd1848ccb8a
Arunanshu Biswas <mydellpc07@gmail.com>
2023-02-14 01:44:55 +0530

    feat(exc): add exception to represent nonexistent records

--- src/app/exc.py
+++ src/app/exc.py
@@ -6,3 +6,10 @@ class NotUniqueError(Exception):
     Exception class representing an error that occurs when a unique constraint
     is violated.
     """
+
+
+class DoesNotExistError(Exception):
+    """
+    Exception class representing an error that occurs when a database record is
+    not found.
+    """
