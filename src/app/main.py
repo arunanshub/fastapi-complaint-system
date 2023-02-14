037ce1775a62ec293e397e131505519900efa1a3
Arunanshu Biswas <mydellpc07@gmail.com>
2023-02-14 18:18:13 +0530

    feat(app): add CORS middleware

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
 from .core import settings
@@ -8,3 +9,10 @@ from .core import settings
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(router, prefix=settings.API_VERSION_URL)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
