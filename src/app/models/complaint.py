74080bfd1e67d0bb437694e1dbc59fd1225399fa
Arunanshu Biswas <mydellpc07@gmail.com>
2023-02-19 16:18:29 +0530

    feat(complaint): store amount as a constrained decimal

--- src/app/models/complaint.py
+++ src/app/models/complaint.py
@@ -1,20 +1,30 @@
 # isort: dont-add-imports
 
+import typing
 from datetime import datetime  # noqa: TC003
 from typing import Optional
 
-from pydantic import HttpUrl  # noqa: TC002
+from pydantic import HttpUrl, condecimal  # noqa: TC002
 from sqlmodel import Column, Field, Relationship, SQLModel, Text, func
 
 from .base import SQLBase
 from .enums import ComplaintStatus
+
+if typing.TYPE_CHECKING:
+    from decimal import Decimal
+
+    class Monetary(Decimal):
+        ...
+
+else:
+    Monetary = condecimal(ge=0, max_digits=19, decimal_places=4)
 
 
 class ComplaintBase(SQLModel):
     title: str = Field(max_length=120)
     description: str = Field(sa_column=Column(Text(), nullable=False))
     photo_url: HttpUrl
-    amount: float
+    amount: Monetary
     created_at: datetime | None = Field(
         nullable=False,
         sa_column_kwargs={
@@ -33,14 +43,14 @@ class ComplaintCreate(SQLModel):
     title: str = Field(max_length=120)
     description: str
     photo_url: HttpUrl
-    amount: float
+    amount: Monetary
 
 
 class ComplaintUpdate(SQLModel):
     title: str | None = Field(default=None, max_length=120)
     description: str | None = None
     photo_url: HttpUrl | None = None
-    amount: float | None = None
+    amount: Monetary | None = None
     status: ComplaintStatus | None = None
 
 
@@ -49,7 +59,7 @@ class ComplaintRead(SQLModel):
     title: str
     description: str
     photo_url: HttpUrl
-    amount: float
+    amount: Monetary
     created_at: datetime
     status: ComplaintStatus
 
@@ -58,7 +68,7 @@ class ComplaintReadWithUser(ComplaintRead):
     user: Optional["UserRead"]
 
 
-class Complaint(SQLBase, ComplaintBase, table=True):  # type: ignore[call-arg]
+class Complaint(SQLBase, ComplaintBase, table=True):
     complainer_id: int = Field(default=None, foreign_key="user.id", index=True)
     user: Optional["User"] = Relationship(
         back_populates="complaints",
